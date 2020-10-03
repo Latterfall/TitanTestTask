d9a16054f6e02fc824e9a035290d168eef29414f
Latterfall <Latterfall@gmail.com>
2020-10-03 12:53:48 +0300

    Init commit

---
+++ src/main/java/app/model/FunctionPair.java
@@ -0,0 +1,11 @@
+package app.model;
+
+public class FunctionPair {
+    public int initialValue;
+    public String function;
+
+    public FunctionPair(int initialValue, String function) {
+        this.initialValue = initialValue;
+        this.function = function;
+    }
+}
